b086933bd2d895a6c175e70b481c571cade25b32
Matheus Guedes <guedesiw@gmail.com>
2018-06-07 22:38:01 -0300

    Initial commit of rotas

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { routing } from './app.routing';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CursosComponent,
+    HomeComponent,
+    LoginComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing
+  ],
+  providers: [CursosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
